89da3429bfc48cbf860c4edb9a2f23dd95f2b71e
5tellar5tar <skold2@outlook.com>
2024-08-29 09:55:39 +0200

    got it to work and tested a bit

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <SFML/Graphics.hpp>
-
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+    auto window = sf::RenderWindow{ { 512u, 128u }, "Fighter" };
     window.setFramerateLimit(144);
-
+    sf::RectangleShape background(sf::Vector2f(512, 128));
+    background.setFillColor(sf::Color::White);
     while (window.isOpen())
     {
         for (auto event = sf::Event{}; window.pollEvent(event);)
@@ -13,9 +15,13 @@ int main()
             {
                 window.close();
             }
+            if (event.type == sf::Event::Resized)
+            {
+                window.setSize(sf::Vector2u(512, 512 * event.size.width / event.size.height));
+            }
         }
-
         window.clear();
+        window.draw(background);
         window.display();
     }
 }
